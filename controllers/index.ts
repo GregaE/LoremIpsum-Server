6222dfe03a11e95480651945aeae83465f76cde6
Eugen Nikolajev <zenicek@gmail.com>
2021-11-26 18:34:20 +0100

    feat: added mock controller

---
+++ controllers/index.ts
@@ -0,0 +1,12 @@
+const db = require('../config/db.json');
+import { Request, Response } from 'express';
+
+export function serveMockData(req: Request, res: Response) {
+  try {
+    console.log(db);
+    res.status(200).send(db);
+  } catch (e) {
+    console.log(e);
+    res.status(500).send(e);
+  }
+}
