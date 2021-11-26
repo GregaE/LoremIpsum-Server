981fc2bfefbc106561d883bbf164b57009b74444
proyev <prots.yevhen@gmail.com>
2021-11-26 18:21:52 +0100

    Add typing cause why not

--- router.ts
+++ router.ts
@@ -1,11 +1,12 @@
 import express from 'express';
+import { Response } from 'express';
 
 const router = express.Router();
 
 router.route('/')
-  .get((_, res) => {
+  .get((_, res: Response): void => {
     res.json(require('./config/db.json'));
-  })
+  });
 
 // router.get("", );
 // router.post("", );
