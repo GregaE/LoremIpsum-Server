6e4a724745bce264152d471942ec75abc68a8d7d
Eugen Nikolajev <zenicek@gmail.com>
2021-11-29 18:23:15 +0100

    refactor: cleaned up all categories and consolidated

--- controllers/categoriesController.ts
+++ controllers/categoriesController.ts
@@ -1,57 +1,81 @@
 import { Request, Response } from 'express';
 import { prisma } from './index';
+import { categoryValidation } from '../interfaces/categories';
 
-export async function getCertificates(
-  req: Request,
-  res: Response
-): Promise<void> {
+export async function getCategory(req: Request, res: Response): Promise<void> {
   try {
-    const { category, userId } = req.params;
-    const certificates = await prisma[category].findMany({
-      where: { userId },
+    const { category, user_id } = req.params;
+    if (!categoryValidation.includes(category)) {
+      res.sendStatus(500).send('Category is not allowed');
+      return;
+    }
+    //@ts-ignore
+    const categoryRecords = await prisma[category].findMany({
+      where: { userId: user_id },
     });
-    res.status(200).send(certificates);
+    const filteredRecords = categoryRecords.map(
+      (cat: { [x: string]: any; userId: string }) => {
+        const { userId, ...record } = cat;
+        return record;
+      }
+    );
+    res.status(200).send(filteredRecords);
   } catch (e) {
     console.log(e);
     res.status(500).send(e);
   }
 }
 
-export async function createCertificate(req: Request, res: Response) {
+export async function createCategoryRecord(req: Request, res: Response) {
   try {
-    const newCertificate = await prisma.certificates.create({
-      data: req.body,
-    });
-    console.log(newCertificate);
-    res.status(201).send(newCertificate);
+    const { category } = req.params;
+    if (categoryValidation.includes(category)) {
+      //@ts-ignore
+      const { userId, ...newRecord } = await prisma[category].create({
+        data: req.body,
+      });
+      res.status(201).send(newRecord);
+      return;
+    }
+    res.status(500).send('Category is not allowed');
   } catch (e) {
     console.log(e);
     res.status(500).send(e);
   }
 }
 
-export async function editCertificate(req: Request, res: Response) {
+export async function editCategoryRecord(req: Request, res: Response) {
   try {
-    const { id } = req.params;
-    const updatedCertificate = await prisma.certificates.update({
-      where: { id },
-      data: req.body,
-    });
-    console.log(updatedCertificate);
-    res.status(201).send(updatedCertificate);
+    const { category, id } = req.params;
+    const { userId, ...data } = req.body;
+    if (categoryValidation.includes(category)) {
+      //@ts-ignore
+      const { userId, ...updatedRecord } = await prisma[category].update({
+        where: { id },
+        data,
+      });
+      res.status(201).send(updatedRecord);
+      return;
+    }
+    res.status(500).send('Category is not allowed');
   } catch (e) {
     console.log(e);
     res.status(500).send(e);
   }
 }
 
-export async function deleteCertificate(req: Request, res: Response) {
+export async function deleteRecord(req: Request, res: Response) {
   try {
-    const { id } = req.params;
-    await prisma.certificates.delete({
-      where: { id },
-    });
-    res.status(204);
+    const { category, id } = req.params;
+    if (categoryValidation.includes(category)) {
+      //@ts-ignore
+      await prisma[category].delete({
+        where: { id },
+      });
+      res.sendStatus(204);
+      return;
+    }
+    res.status(500).send('Category is not allowed');
   } catch (e) {
     console.log(e);
     res.status(500).send(e);
--- router.ts
+++ router.ts
@@ -6,47 +6,11 @@ import {
   updateUser,
 } from './controllers/userController';
 import {
-  getCertificates,
-  createCertificate,
-  editCertificate,
-  deleteCertificate,
-} from './controllers/certificatesController';
-import {
-  getLanguages,
-  createLanguage,
-  editLanguage,
-  deleteLanguage,
-} from './controllers/languagesController';
-import {
-  getEducation,
-  createEducation,
-  editEducation,
-  deleteEducation,
-} from './controllers/educationController';
-import {
-  getWorkExperience,
-  createWorkExperience,
-  editWorkExperience,
-  deleteWorkExperience,
-} from './controllers/workExperienceController';
-import {
-  getSkills,
-  createSkill,
-  editSkill,
-  deleteSkill,
-} from './controllers/skillsController';
-import {
-  getCVs,
-  postNewCV,
-  deleteCV,
-} from './controllers/createdCvsController';
-import {
-  getPersonalInfo,
-  createPersonalInfo,
-  editPersonalInfo,
-  deletePersonalInfo,
-} from './controllers/personalInfoController';
-import { serveMockData } from './controllers/index';
+  createCategoryRecord,
+  deleteRecord,
+  editCategoryRecord,
+  getCategory,
+} from './controllers/categoriesController';
 
 const router = Router();
 
@@ -58,58 +22,9 @@ router.delete('/user/:id', deleteUser);
 
 //categories
 
-router.get('/:category/:userId');
-router.post('/:category');
-router.put('/:category/:id');
-router.delete('/:category/:id');
+router.get('/:category/:user_id', getCategory);
+router.post('/:category', createCategoryRecord);
+router.put('/:category/:id', editCategoryRecord);
+router.delete('/:category/:id', deleteRecord);
 
-// certificates
-
-router.get('/certificate/:userId', getCertificates);
-router.post('/certificate', createCertificate);
-router.put('/certificate/:id', editCertificate);
-router.delete('/certificate/:id', deleteCertificate);
-
-// education
-
-router.get('/education/:userId', getEducation);
-router.post('/education', createEducation);
-router.put('/education/:id', editEducation);
-router.delete('/education/:id', deleteEducation);
-
-// work experience
-
-router.get('/workExperience/:userId', getWorkExperience);
-router.post('/workExperience', createWorkExperience);
-router.put('/workExperience/:id', editWorkExperience);
-router.delete('/workExperience/:id', deleteWorkExperience);
-
-//languages
-
-router.get('/languages/:userId', getLanguages);
-router.post('/languages', createLanguage);
-router.put('/languages/:id', editLanguage);
-router.delete('/languages/:id', deleteLanguage);
-
-// skills
-
-router.get('/skills/:userId', getSkills);
-router.post('/skills', createSkill);
-router.put('/skills/:id', editSkill);
-router.delete('/skills/:id', deleteSkill);
-
-// CVs
-
-router.get('/createdcvs/:userId', getCVs);
-router.post('/createdcvs/', postNewCV);
-router.delete('/createdcvs/:id', deleteCV);
-
-// profile
-
-router.get('/personalInfo/:userId', getPersonalInfo);
-router.post('/personalInfo', createPersonalInfo);
-router.put('/personalInfo/:id', editPersonalInfo);
-router.delete('/personalInfo/:id', deletePersonalInfo);
-
-router.get('/mock', serveMockData);
 export default router;
