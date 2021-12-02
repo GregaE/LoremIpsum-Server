53895e8e1f712311e1835c0aa0f98448d3e56748
Tomazzo Antonioli <tomazzo10@hotmail.com>
2021-12-02 16:06:26 +0100

    updating

--- index.ts
+++ index.ts
@@ -33,22 +33,6 @@ app
   )
   .use(router);
 
-app.use(
-  session({
-    name: 'sid',
-    saveUninitialized: false,
-    resave: false,
-    secret: SECRET ? SECRET : '',
-    cookie: {
-      maxAge: 1000 * 60 * 60, // 1hr
-      sameSite: true,
-      httpOnly: false,
-      // ----> we would want to set secure=true in a production environment <----
-      secure: false,
-    },
-  })
-)
-
 const PORT = process.env.SERVER_PORT || 3001;
 
 (async function () {
